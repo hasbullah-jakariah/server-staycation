c9a1c5ef44835bf51c9d382eaccee77971bf7fcd
hasbullah-jakariah <114131640+hasbullah-jakariah@users.noreply.github.com>
2023-01-28 02:29:45 -0800

    typo in file auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,13 @@
+
 const isLogin = (req, res, next) => {
-    if(req.session.user == null || req.session.user == undefined){
-        req.flash("alertMessage", "session telah habis!!!");
-        req.flash("alertStatus", "danger");
-        res.redirect("/admin/signin");
-    }else{
-        next();
+    if (req.session.user == null || req.session.user == undefined) {
+      req.flash('alertMessage', 'Session telah habis silahkan signin kembali!!');
+      req.flash('alertStatus', 'danger');
+      res.redirect('/admin/signin');
+    } else {
+      next();
     }
-}
-module.exports = isLogin;
+  }
+  
+  module.exports = isLogin;
+
